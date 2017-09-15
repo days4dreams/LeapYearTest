87c047422c8bf12bde01ad492389e003e26eda2d
lauren <lauren@LAURENJANEYBC1C>
2017-09-15 17:03:23 +0100

    Add project files.

---
+++ LeapYear/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Test TestGivenNumber = new Test();
+
+            Console.WriteLine("Test for Leap Year. Please enter a year");
+            int GivenYear = Convert.ToInt32(Console.ReadLine());
+
+            bool LeapYearStatus = TestGivenNumber.CalculateLeapStatus(GivenYear);
+
+            if (LeapYearStatus == true)
+            {
+                Console.WriteLine(GivenYear + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(GivenYear + " is not a leap year");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LeapYear/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    public class Test
+    {
+        bool LeapStatus = true;
+        public bool CalculateLeapStatus(int givenYear)
+        {
+
+            if (givenYear % 4 == 0)
+            {
+                if (givenYear % 100 == 0)
+                {
+                    if (givenYear % 400 == 0)
+                    {
+                        LeapStatus = true;
+                    }
+                    else
+                    {
+                        LeapStatus = false;
+                    }
+                }
+                else
+                {
+                    LeapStatus = true;
+                }
+            }
+
+            else
+            {
+                LeapStatus = false;
+            }
+
+            return LeapStatus;
+        }
+    }
+}
